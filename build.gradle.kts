de7b1100b5628ea673dd743bea630ece3101f12b
Ivan Bialotski <murfrik666@gmail.com>
2025-03-15 18:24:59 +0400

    feat: initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.3"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.sporty"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(23)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+
+	implementation("io.r2dbc:r2dbc-postgresql:0.8.13.RELEASE")
+	implementation("org.liquibase:liquibase-core")
+	implementation("org.postgresql:postgresql:42.7.5")
+
+	implementation("org.mapstruct:mapstruct:1.6.3")
+
+	compileOnly("org.projectlombok:lombok:1.18.36")
+	annotationProcessor("org.projectlombok:lombok:1.18.36")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+
+	// tests
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
